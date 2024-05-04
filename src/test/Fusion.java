b4b9655b8101414fbf63f8bcebe1609b58531199
PRADNYAAUTADE <pradnyaautade9698@gmail.com>
2024-05-04 16:17:06 +0530

    tcs

---
+++ src/test/Fusion.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Fusion {
+
+	public static void main(String[] args) {
+		System.out.println("My full name is Pradnya Autade");
+		
+		System.out.println("Pune");
+	}
+	}
+		
